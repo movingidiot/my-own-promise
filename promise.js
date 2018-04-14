97c6002eb12395873d05799f2d47dad8bc93dbe1
DESKTOP-ORQOQ33\hasee <b371506395@gmail.com>
2018-04-14 13:14:06 +0800

    passed PROMISE/A+ test suite

--- promise.js
+++ promise.js
@@ -1,6 +1,11 @@
+const symbols = {
+    state: Symbol('PromiseState'),
+    value: Symbol('PromiseValue'),
+};
+
 function syncTask(func, ...args) {
-    //process.nextTick(func, args);
-    setTimeout(func, 0, args);
+    process.nextTick(func, args);
+    //setTimeout(func, 0, args);
 }
 
 function resolutionProcedureFactory(targetPromise, toResolve, toReject) {
@@ -8,31 +13,32 @@ function resolutionProcedureFactory(targetPromise, toResolve, toReject) {
         if (targetPromise === value) {
             toReject(new TypeError('not allowed to return self promise'));
         } else
-            if (typeof value === 'object' || typeof value === 'function') {
-                let then;
+        if (value && (typeof value === 'object' || typeof value === 'function')) {
+            let then;
+            try {
+                then = value.then;
+            } catch (err) {
+                toReject(err);
+                return;
+            }
+            if (typeof then === 'function') {
+                let called = false;
                 try {
-                    then = value.then;
+                    then.call(value, y => (!called) && (called = true) && resolutionProcedure(y), r => (!called) && (called = true) && toReject(r));
                 } catch (err) {
-                    toReject(err);
-                    return;
+                    (!called) && (called = true) && toReject(err);
                 }
-                    if (typeof then === 'function')
-                        try {
-                            then.call(value, y => resolutionProcedure(y), r => toReject(r));
-                        } catch (err) {
-                            toReject(err);
-                        }
-                    else {
-                        toResolve(value);
-                    }
             } else {
                 toResolve(value);
             }
+        } else {
+            toResolve(value);
+        }
     }
     return resolutionProcedure;
 }
 
-class Promise{
+class Promise {
     constructor(executor) {
         if (typeof executor !== 'function') {
             throw new TypeError('executor must be function');
@@ -40,29 +46,29 @@ class Promise{
         const onFulfilled = [];
         const onRejected = [];
         const self = this;
-        const resolve = function(value) {
-            if (self.state === 'pending') {
-                self.state = 'fulfilled';
-                self.value = value;
-                self.broadcast(onFulfilled,value);
+        const resolve = function (value) {
+            if (self[symbols.state] === 'pending') {
+                self[symbols.state] = 'fulfilled';
+                self[symbols.value] = value;
+                self.broadcast(onFulfilled, value);
             } else {
                 //throw new Error('this promise has finished');
             }
         }
-        const reject = function(reason) {
-            if (self.state === 'pending') {
-                self.state = 'rejected';
-                self.value = reason;
-                self.broadcast(onRejected,reason);
+        const reject = function (reason) {
+            if (self[symbols.state] === 'pending') {
+                self[symbols.state] = 'rejected';
+                self[symbols.value] = reason;
+                self.broadcast(onRejected, reason);
             } else {
                 //throw new Error('this promise has finished');
-            }    
+            }
         }
-        this.state = 'pending';
+        this[symbols.state] = 'pending';
         this.onFulfilled = onFulfilled;
         this.onRejected = onRejected;
         //syncTask(() => {
-            executor(resolve, reject);
+        executor(resolve, reject);
         //});
     }
 
@@ -78,10 +84,9 @@ class Promise{
                 try {
                     resolutionProcedure(onFulfilled(value));
                 }
-                catch (err) {
-                    rej(err);
-                }
-            else
+            catch (err) {
+                rej(err);
+            } else
                 res(value);
         };
         const wrapOnRejected = function (value) {
@@ -90,27 +95,27 @@ class Promise{
                     resolutionProcedure(onRejected(value));
                 } catch (err) {
                     rej(err);
-                }    
+                }
             else
                 rej(value);
         }
-        
-        if (this.state === 'fulfilled')
-            this.broadcast([wrapOnFulfilled], this.value);
+
+        if (this[symbols.state] === 'fulfilled')
+            this.broadcast([wrapOnFulfilled], this[symbols.value]);
         else
             this.onFulfilled.push(wrapOnFulfilled);
-        if (this.state === 'rejected')
-            this.broadcast([wrapOnRejected], this.value);
+        if (this[symbols.state] === 'rejected')
+            this.broadcast([wrapOnRejected], this[symbols.value]);
         else
             this.onRejected.push(wrapOnRejected);
         return ans;
     }
 
-    broadcast(funcArr,value) {
+    broadcast(funcArr, value) {
         const self = this;
         syncTask(function () {
             funcArr.forEach(function (func) {
-                    func(value);
+                func(value);
             });
             funcArr = [];
         });
@@ -162,20 +167,31 @@ promise.then(null, function () {
     console.log(value === sentinel3);
 });
 */
-
-const promise = new Promise((resolve, reject) => resolve(123));
-promise.then((value) => {
-    return {
-        then: (onRes, onRej) => {
-            onRes('asdf');
-        }
-    }
-}, () => {
-    
-});
-
-
 /*
+const promise = new Promise((resolve, reject) => resolve(1));
+promise.then((value) => {
+    const x = {
+        then: function (resolvePromise) {
+            const y = {
+                then: function (onFulfilled) {
+                    onFulfilled({
+                        then: function (onFulfilled) {
+                            setTimeout(onFulfilled, 0, 'asdf');
+                        }
+                    });
+                    throw 'oij';
+                }
+            }
+            resolvePromise(y);
+        }
+    };
+    return x;
+}, () => {
+    console.log(123);
+}).then((value)=>console.log(value));*/
+
+
+
 test(adapter, function (err) {
     console.dir(err);
-});*/
+});
